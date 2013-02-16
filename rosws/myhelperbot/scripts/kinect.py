f50b4e062c0e11b9c1c8be7506fd4faf3b3560a4
DouglasSherk <github@sherk.me>
2013-02-15 21:50:09 -0500

    Add some example code

--- rosws/myhelperbot/scripts/kinect.py
+++ rosws/myhelperbot/scripts/kinect.py
@@ -1,21 +1,50 @@
-#!/usr/bin/env python
-import roslib; roslib.load_manifest('myhelperbot')
+import roslib
+roslib.load_manifest('ros_skeleton_recorder')
 import rospy
-from std_msgs.msg import String
+import tf
 
+BASE_FRAME = '/openni_depth_frame'
+FRAMES = [
+    'head',
+    'neck',
+    'torso',
+    'left_shoulder',
+    'left_elbow',
+    'left_hand',
+    'left_hip',
+    'left_knee',
+    'left_foot',
+    'right_shoulder',
+    'right_elbow',
+    'right_hand',
+    'right_hip',
+    'right_knee',
+    'right_foot',
+]
+LAST = rospy.Duration()
 
-def talker():
-    pub = rospy.Publisher('chatter', String)
-    rospy.init_node('talker')
-    while not rospy.is_shutdown():
-        str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(str)
-        pub.publish(String(str))
-        rospy.sleep(1.0)
+class Kinect:
 
+    def __init__(self, name='kinect_listener', user=1):
+        rospy.init_node(name, anonymous=True)
+        self.listener = tf.TransformListener()
+        self.user = user
 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    def get_posture(self):
+        """Returns a list of frames constituted by a translation matrix
+        and a rotation matrix.
+
+        Raises IndexError when a frame can't be found (which happens if
+        the requested user is not calibrated).
+        """
+        try:
+            frames = []
+        for frame in FRAMES:
+            trans, rot = self.listener.lookupTransform(BASE_FRAME,
+        "/%s_%d" % (frame, self.user), LAST)
+            frames.append((trans, rot))
+        return frames
+        except (tf.LookupException,
+                tf.ConnectivityException,
+                tf.ExtrapolationException):
+            raise IndexError

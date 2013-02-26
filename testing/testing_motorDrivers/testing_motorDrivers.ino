a84217c9cbe9f7b2a5ad81ce94cbf1269aca497e
James Anselm <james@anselm.ca>
2013-02-25 21:24:21 -0500

    updated testing files for imu, compass, and motor drivers.

--- testing/testing_motorDrivers/testing_motorDrivers.ino
+++ testing/testing_motorDrivers/testing_motorDrivers.ino
@@ -1,43 +1,43 @@
 #include "MC33926MotorShield.h"
 
-MC33926MotorShield m1(31, 23, 25, 3, 27, 29);
-MC33926MotorShield m2(30, 22, 24, 2, 26, 28);
+MC33926MotorShield motorR(31, 23, 25, 3, 27, 29); //right
+MC33926MotorShield motorL(30, 22, 24, 2, 26, 28); //left
 
 void setup() {
-   m1.init();
-   m2.init();
+   motorR.init();
+   motorL.init();
    Serial.begin(38400);
 }
 
 void loop() {
-   /*m1.setSpeed(200);
+   /*motorR.setSpeed(200);
    Serial.print("D1: ");
-   Serial.print(digitalRead(m1._D1));
+   Serial.print(digitalRead(motorR._D1));
    Serial.print("  EN: ");
-   Serial.print(digitalRead(m1._EN));
+   Serial.print(digitalRead(motorR._EN));
    Serial.print("  IN1: ");
-   Serial.print(digitalRead(m1._IN1));
+   Serial.print(digitalRead(motorR._IN1));
    Serial.print("  IN2: ");
-   Serial.print(digitalRead(m1._IN2));
+   Serial.print(digitalRead(motorR._IN2));
    Serial.print("  INV: ");
-   Serial.print(digitalRead(m1._INV));
+   Serial.print(digitalRead(motorR._INV));
    Serial.print("\n");*/
    
    for(int value=0; value<255; value += 10){
-      m1.setSpeed(value);
-      m2.setSpeed(value);
+      motorR.setSpeed(value);
+      motorL.setSpeed(value);
       Serial.println(value);
       delay(200);
    }   
    for(int value=255; value>-255; value -= 10){
-      m1.setSpeed(value);
-      m2.setSpeed(value);
+      motorR.setSpeed(value);
+      motorL.setSpeed(value);
       Serial.println(value);
       delay(200);
    }
    for(int value=-255; value<0; value += 10){
-      m1.setSpeed(value);
-      m2.setSpeed(value);
+      motorR.setSpeed(value);
+      motorL.setSpeed(value);
       Serial.println(value);
       delay(200);
    }  
